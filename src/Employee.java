9ed7a389090542e7eedb7c1e97173b2292660359
gface34rus <89259112970@mail.ru>
2024-06-24 14:55:46 +0300

    basic difficulty is done

---
+++ src/Employee.java
@@ -0,0 +1,94 @@
+import java.util.Objects;
+
+public class Employee {
+    private static int counter = 1;
+    private final int id;
+    private String name;
+    private String patronymic;
+    private String surname;
+    private double salary;
+    private int department;
+
+    public Employee(String name, String patronymic, String surname, int department, double salary) {
+        this.id = counter++;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFullName(Employee employee) {
+        return "ФИО-" + employee.getSurname() + " " + employee.getName() + " " + employee.getPatronymic();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id &&
+                Double.compare(salary, employee.salary) == 0 &&
+                department == employee.department &&
+                Objects.equals(name, employee.name) &&
+                Objects.equals(patronymic, employee.patronymic) &&
+                Objects.equals(surname, employee.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, patronymic, surname, salary, department);
+    }
+
+    @Override
+    public String toString() {
+        return "id-" + id + " ФИО-" + surname +
+                " " + name + " " + patronymic + " Отдел-" +
+                department + " Зарплата-" + salary;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
 public class Main {
+    static Employee[] employees = new Employee[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        employees[0] = new Employee("Павел", "Александрович", "Никитин", 1, 22400);
+        employees[1] = new Employee("Евгений", "Юрьевич", "Романов", 2, 44700);
+        employees[2] = new Employee("Кирилл", "Федорович", "Донцов", 3, 78250);
+        employees[4] = new Employee("Николай", "Васильевич", "Нелюбов", 4, 45780);
+        employees[5] = new Employee("Денис", "Сергеевич", "Прищепа", 5, 68900);
+        employees[6] = new Employee("Иван", "Юрьевич", "Пяткин", 1, 98200);
+        employees[7] = new Employee("Илья", "Сергеевич", "Сафронов", 2, 75600);
+        employees[8] = new Employee("Милана", "Юрьевна", "Елисеева", 3, 98700);
+        employees[9] = new Employee("Алевтина", "Ивановна", "Пестерева", 4, 99900);
+        printAllEmployees();
+        System.out.println("Общая сумма трат на зарплату в месяц - " + calculateTotalSalary());
+        System.out.println("Величина средней зарплаты составила - " + calculateAverageSalary());
+        System.out.println("Сотрудник с минимальной зарплатой - " + findEmployeeWithMinSalary());
+        System.out.println("Сотрудник с максимальной зарплатой - "+findEmployeeWithMaxSalary());
+        printAllFullNames();
+
+    }
+
+    public static void printAllEmployees() {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee.toString());
+            }
+        }
+    }
+
+    public static double calculateTotalSalary() {
+        double totalSalary = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                totalSalary += employee.getSalary();
+            }
+        }
+        return totalSalary;
+    }
+
+    public static Employee findEmployeeWithMinSalary() {
+        Employee minSalaryEmployee = employees[0];
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() < minSalaryEmployee.getSalary()) {
+                minSalaryEmployee = employee;
+            }
+        }
+        return minSalaryEmployee;
+    }
+
+    public static Employee findEmployeeWithMaxSalary() {
+        Employee maxSalaryEmployee = employees[0];
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() > maxSalaryEmployee.getSalary()) {
+                maxSalaryEmployee = employee;
+            }
+        }
+        return maxSalaryEmployee;
+    }
+
+    public static double calculateAverageSalary() {
+        double totalSalary = calculateTotalSalary();
+        int numEmployees = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                numEmployees++;
+            }
+        }
+        return totalSalary / numEmployees;
+    }
+
+    public static void printAllFullNames() {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee.getFullName(employee));
+            }
+        }
     }
 }
